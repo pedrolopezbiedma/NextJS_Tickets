a175f92b61ed69fbb557679656ae7286c5ebfa61
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-16 22:37:04 +0100

    Create a custom not found page for the ticket details screen

---
+++ app/tickets/[id]/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const NotFound = () => {
+    return(
+        <main className="text-center">
+            <h2 className="text-3xl">There was a problem</h2>
+            <p>We could not find the ticket you were looking for</p>
+            <p><Link href='/tickets'>Back to tickets</Link></p>
+        </main>
+    )
+}
+
+export default NotFound
